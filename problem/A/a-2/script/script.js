d1d9ea2182d8e8115978ed9d169f53853a23af6b
임예은 <ifg.035@daum.net>
2024-06-03 14:31:12 +0900

    main

---
+++ problem/A/a-2/script/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".nav>ul>li").mouseover(function(){
+        $(".nav>ul>li>ul").stop().slideDown()
+        $("#header").addClass("on");
+    })
+    $(".nav>ul>li").mouseout(function(){
+        $(".nav>ul>li>ul").stop().slideUp()
+        $("#header").removeClass("on");
+    })
+})
