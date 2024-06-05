094b906bd7642208d88c43613b7fee6f07872e52
임예은 <ifg.035@daum.net>
2024-06-05 15:17:35 +0900

    C-4 end

--- problem/C/c-4/script/script.js
+++ problem/C/c-4/script/script.js
@@ -1,14 +1,13 @@
 $(function(){
     //nav
-    // $(".nav>ul>li").mouseover(function(){
-    //     $(".nav>ul>li>ul").stop().slideDown();
-    // })
-    // $(".nav>ul>li").mouseout(function(){
-    //     $(".nav>ul>li>ul").stop().slideUp();
-    // })
+   $(".nav>ul>li").mouseover(function(){
+    $(this).children(".sub").stop().fadeIn()
+   })
+   $(".nav>ul>li").mouseout(function(){
+    $(this).children(".sub").stop().fadeOut()
+   })
 
-
-    //slide
+    slide
     function slide(){
         $(".slide ul").animate({left:"-100%"},1000,function(){
             $(".slide ul").append($(".slide ul li").first())
