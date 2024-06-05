9f0147e489c3ce815387725b77e29d508e08d2b6
임예은 <ifg.035@daum.net>
2024-06-05 16:43:37 +0900

    D-2 baner end

---
+++ problem/D/D-2/script/script.js
@@ -0,0 +1,19 @@
+$(function(){
+    //nav
+    $(".nav>ul>li").mouseover(function(){
+        $(this).children(".sub").stop().slideDown()
+    })
+    $(".nav>ul>li").mouseout(function(){
+        $(this).children(".sub").stop().slideUp()
+    })
+
+    //slide
+    // function slide(){
+    //     $(".slide ul").animate({left:"-100%"},1000,function(){
+    //         $(".slide ul").append($(".slide ul li").first());
+    //         $(".slide ul").css({left:0})
+    //     })
+    // }
+    // setInterval(slide,2000)
+
+})
