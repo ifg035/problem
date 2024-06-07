a6631c1ce77a60ebd83efd3371b45ed126f64922
임예은 <ifg.035@daum.net>
2024-06-07 12:03:24 +0900

    ss

--- problem/F/script/scritp.js
+++ problem/F/script/scritp.js
@@ -1,11 +1,11 @@
 $(function(){
     //nav
-    // $(".nav>ul>li").mouseover(function(){
-    //     $(this).children(".sub").stop().slideDown()
-    // })
-    // $(".nav>ul>li").mouseout(function(){
-    //     $(this).children(".sub").stop().slideUp()
-    // })
+    $(".nav>ul>li").mouseover(function(){
+        $(this).children(".sub").stop().slideDown()
+    })
+    $(".nav>ul>li").mouseout(function(){
+        $(this).children(".sub").stop().slideUp()
+    })
 
     //slide
     var t=0;
