77c7336c4255917fc37cbf23d8d4ea723edf649e
임예은 <ifg.035@daum.net>
2024-06-07 11:42:12 +0900

    F-nav

---
+++ problem/F/script/scritp.js
@@ -0,0 +1,33 @@
+$(function(){
+    //nav
+    // $(".nav>ul>li").mouseover(function(){
+    //     $(this).children(".sub").stop().slideDown()
+    // })
+    // $(".nav>ul>li").mouseout(function(){
+    //     $(this).children(".sub").stop().slideUp()
+    // })
+
+    //slide
+    var t=0;
+
+    function slide(){
+        if(t<2){
+            t++;
+        }else{
+            t=0;
+        }
+
+        $(".slide ul li").fadeIn()
+        $(".slide ul li").eq(t).fadeOut()
+    }
+    setInterval (slide,2000)
+
+    //popup
+    $(".pp").click(function(){
+        $(".popup").show();
+    })
+    $("button").click(function(){
+        $(".popup").hide();
+    })
+
+})
