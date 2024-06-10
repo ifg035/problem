456877772d094e7b1af31ce8d5bffb9cfe8157c6
임예은 <ifg.035@daum.net>
2024-06-10 15:39:41 +0900

    D-4 end

--- problem/D/D-4/script/scritp.js
+++ problem/D/D-4/script/scritp.js
@@ -6,4 +6,39 @@ $(function(){
     $(".nav>ul>li").mouseout(function(){
         $(".sub,.bg").stop().fadeOut();
     })
+
+    //slide
+
+    var t =0
+    function slide(){
+        if(t<2){
+            t++;
+        }else{
+            t=0;
+        }
+        $(".slide>ul>li").fadeOut();
+        $(".slide>ul>li").eq(t).fadeIn();
+    }
+    setInterval (slide,2000)
+
+    //tabmenu
+    $(".tabmenu>li").click(function(){
+        $(".tabmenu>li").removeClass("on");
+        $(this).addClass("on");
+
+        var i = $(this).index()
+
+        console.log(i)
+
+        $(".tabcon").hide()
+        $(".tabcon").eq(i).show()
+    })
+
+    //popup
+    $(".pp").click(function(){
+        $(".popup").show()
+    })
+    $("button").click(function(){
+        $(".popup").hide()
+    })
 })
